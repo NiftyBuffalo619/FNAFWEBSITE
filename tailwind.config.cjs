e7a3c0b3d61ec61ddd70f9c30f9a848642ba2aad
NiftyBuffalo619 <polaclukes@outlook.cz>
2023-04-17 20:29:41 +0200

    improved design

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+//const colors = require('tailwindcss/colors')
 
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
 	},
+	/*colors: {
+	  blue: colors.blue,
+      green: colors.green,
+      pink: colors.pink,
+      purple: colors.purple,
+      orange: colors.orange,
+      red: colors.red,
+      yellow: colors.yellow,
+	},*/
 	plugins: [],
 }
